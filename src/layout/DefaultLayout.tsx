a6b2123bc50ff419b81029d2d6ad73900fb2d170
edd.dev <soka_117@live.com>
2025-06-18 11:53:12 -0600

    feat: the layout component is updated to display the current page

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,13 +1,36 @@
-import Sidebar from '../core/Sidebar'
-import SectionIndicator from '../core/SectionIndicator'
+import type { ReactNode, FC } from 'react'
+import { useLocation } from 'react-router-dom'
 
-const DefaultLayout = () => {
+import Sidebar from '../core/sidebar/Sidebar'
+import SectionIndicator from '../core/sectionIndicator/SectionIndicator'
+
+interface DefaultLayoutProps {
+  children: ReactNode
+}
+
+const DefaultLayout: FC<DefaultLayoutProps> = ({ children }) => {
+  const location = useLocation()
+
+  const getCurrentSection = () => {
+    switch (location.pathname) {
+      case '/':
+        return 'HOME'
+      case '/about':
+        return 'ABOUT'
+      default:
+        return ''
+    }
+  }
+
   return (
-    <>
+    <div className="lg:flex lg:p-0 p-8">
       <Sidebar />
-      <div>Main Content</div>
-      <SectionIndicator />
-    </>
+
+      <div className="flex flex-1 justify-center items-center relative overflow-hidden">
+        <main>{children}</main>
+        <SectionIndicator current={getCurrentSection()} />
+      </div>
+    </div>
   )
 }
 
