c8e6661ecde5125737ccd2e32a274787dae86562
edd.dev <soka_117@live.com>
2025-06-19 15:31:59 -0600

    feat: the sections contact me my experience and my projects are created

---
+++ src/sections/myExperienceSection/MyExperienceSection.tsx
@@ -0,0 +1,16 @@
+const MyExperienceSection = () => {
+  return (
+    <div
+      id="experience"
+      data-section="experience"
+      className="text-white [animation:var(--animation-fade-in)] scroll-section"
+    >
+      <h1 className="text-4xl font-semibold mb-4 tracking-[.10rem]">
+        My Experience
+      </h1>
+      <p>Here you can find a list of my experience.</p>
+    </div>
+  )
+}
+
+export default MyExperienceSection
