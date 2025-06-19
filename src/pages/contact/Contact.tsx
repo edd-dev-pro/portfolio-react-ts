9aef4d28ff2a308b1717f05dcec5baa6972b8498
edd.dev <soka_117@live.com>
2025-06-19 15:28:42 -0600

    feat: the pages contact experience and projects are created

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from '../../layout/DefaultLayout'
+import { ContactMeSection } from '../../sections'
+
+const Contact = () => {
+  return (
+    <DefaultLayout>
+      <ContactMeSection />
+    </DefaultLayout>
+  )
+}
+
+export default Contact
