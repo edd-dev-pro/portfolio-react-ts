aebfa1f6982825f08597d1cdfedeae547c689faa
Eduardo Martínez <soka_117@live.com>
2026-09-07 23:25:04 -0600

    chore: the section indicator styles are modified

--- src/core/sectionIndicator/SectionIndicator.tsx
+++ src/core/sectionIndicator/SectionIndicator.tsx
@@ -8,7 +8,7 @@ const SectionIndicator: FC<sectionIndicatorProps> = ({ current }) => {
   current = current.replace('/', '') || 'home'
 
   return (
-    <div id="section-indicator" className="absolute bottom-9 right-3">
+    <div id="section-indicator" className="absolute bottom-22 right-0">
       <div
         id="section-indicator"
         className="lg:flex flex-col justify-between items-center mr-0 lg:mr-6 h-24 text-white"
@@ -17,7 +17,7 @@ const SectionIndicator: FC<sectionIndicatorProps> = ({ current }) => {
           <div className="w-[2px] rounded-4xl h-10 bg-muted opacity-[.25]" />
           <div className="w-2 h-2 rounded-full bg-white [animation:var(--animation-bounce-y)]" />
         </div>
-        <div className="rotate-90 origin-center text-[12px] font-mono tracking-[.25rem] uppercase whitespace-nowrap">
+        <div className="rotate-90 origin-center text-[12px] font-mono tracking-[.25rem] uppercase whitespace-nowrap max-w-12">
           {current}
         </div>
       </div>
